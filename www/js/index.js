6f9606c217f01dbf081edf65dc9a26c4aeaa23e9
Andre <sosabio@gmail.com>
2014-07-31 15:20:34 -0300

    app.

--- www/js/index.js
+++ www/js/index.js
@@ -44,24 +44,15 @@ var app = {
     contato: function(){
       var options = new ContactFindOptions();
       options.filter = "";
-      var filter = ["displayName", "addresses"];
-      navigator.contacts.find(filter, onSuccessContato, onErrorContato, options);
+      options.multiple=true;    // return multiple results
+      var filter = ["displayName"];
+      navigator.contacts.find(filter, app.onSuccessContato, app.onErrorContato, options);
     },
 
     onSuccessContato: function(contacts) {
-        // display the address information for all contacts
-        for (var i = 0; i < contacts.length; i++) {
-            for (var j = 0; j < contacts[i].addresses.length; j++) {
-                document.getElementById('contatos').innerHTML = ("Pref: "           + contacts[i].addresses[j].pref          + "\n" +
-                      "Type: "           + contacts[i].addresses[j].type          + "\n" +
-                      "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
-                      "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
-                      "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
-                      "Region: "         + contacts[i].addresses[j].region        + "\n" +
-                      "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
-                      "Country: "        + contacts[i].addresses[j].country);
-            }
-        }
+      for (var i = 0; i < contacts.length; i++) {
+        alert(contacts[i].displayName);
+      }
     },
     
     onErrorContato: function(contactError) {
